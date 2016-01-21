a88211c836b48c62b6d6997930809d19a40e7234
ashekineiko <ashekinrozon@mybihs.org>
2016-01-21 22:58:38 +0000

    flickr search

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,1 +1,13 @@
 // YOUR CODE GOES HERE
+$(document).ready(function (){
+    $("#search-btn").click(function(){
+        var khalu = $("#search-query").val();
+        console.log(khalu);
+        
+        searchFlickr(khalu, function(alu){
+        console.log(alu);
+        
+        });
+            
+    });
+});
